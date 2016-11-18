7788664a819448c86500c923daac5a0e51160991
Steve Weiss <weissst@mail.gvsu.edu>
2016-11-18 15:05:48 -0500

    Updated isNone helper comment.

--- app/helpers/is-none.js
+++ app/helpers/is-none.js
@@ -2,6 +2,6 @@ export function isNone(value) {
   return (
     value === null ||
     value === undefined ||
-    (isNaN(value) && value !== value) // NaN is the only object not equal to itself in js.
+    (isNaN(value) && value !== value) // NaN is the only js object not equal to itself.
   )
 }
