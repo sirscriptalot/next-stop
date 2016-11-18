7887cbffaa2a9e0764222575f737920015cda7c9
Steve Weiss <weissst@mail.gvsu.edu>
2016-11-18 10:29:02 -0500

    Added initial webpack config and babel configs.

---
+++ app/index.js
@@ -0,0 +1,5 @@
+"use strict"
+
+require("./styles/index.scss")
+
+alert('Hello World')
---
+++ config/webpack.js
@@ -0,0 +1,35 @@
+"use strict"
+
+var path              = require("path")
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+module.exports = {
+  entry: [
+    path.join(__dirname, "../app/index.js"),
+    path.join(__dirname, "../app/styles/index.scss"),
+  ],
+  output: {
+    path: path.join(__dirname, "../public"),
+    filename: "bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/, // .js or .jsx
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s?css$/, // .css or .scss (sass)
+        loader: ExtractTextPlugin.extract("style", "css!sass"),
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      },
+    ]
+  },
+  plugins: [
+      new ExtractTextPlugin("bundle.css"),
+  ]
+}
